58d71937be26e693cffda89304258b22c74fe7dd
KUCKLU <kuck1u@visualive.jp>
2016-10-13 03:09:06 +0900

    First commit.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * WordPress main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link       http://codex.wordpress.org/Template_Hierarchy
+ * @package    WordPress
+ * @subpackage VisuAlive
+ * @author     Copyright (C) 2016  KUCKLU and VisuAlive.
+ *             This program is free software: you can redistribute it and/or modify
+ *             it under the terms of the GNU General Public License as published by
+ *             the Free Software Foundation, either version 3 of the License, or
+ *             (at your option) any later version.
+ *             This program is distributed in the hope that it will be useful,
+ *             but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *             GNU General Public License for more details.
+ *             You should have received a copy of the GNU General Public License
+ *             along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+
