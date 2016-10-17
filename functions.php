a18ba1299c89b335e2a852cf943d76655bb115ec
KUCKLU <kuck1u@visualive.jp>
2016-10-17 23:45:40 +0900

    Doc コメントの修正。

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
  *             Functions that are not pluggable (not wrapped in function_exists()) are
  *             instead attached to a filter or action hook.
  *             For more information on hooks, actions, and filters,
- *             {@link https://codex.wordpress.org/Plugin_API}
+ * @link       https://codex.wordpress.org/Plugin_API
  * @package    WordPress
  * @subpackage VisuAlive
  * @author     KUCKLU
--- incs/class-theme-init.php
+++ incs/class-theme-init.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * VisuAlive functions and definitions
+ * VisuAlive functions and definitions.
+ *
  * Set up the theme and provides some helper functions, which are used in the
  * theme as custom template tags. Others are attached to action and filter
  * hooks in WordPress to change core functionality.
